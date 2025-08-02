43c43e52d0e4a1f99227f245fdd72f4ce76746ca
Nikhil Sehgal <nikhil.sehgal.28@gmail.com>
2025-08-02 09:59:50 +0100

    overdamped langevin add

--- optimisers.py
+++ optimisers.py
@@ -1,1 +1,29 @@
-print('hello world')
+import torch
+import numpy as np
+
+class OverdampedLangevin:
+    def __init__(self,
+                 step_size,
+                 beta,
+                 num_particles,
+                 dimension,
+                 potential_function,
+                 is_valid = None,
+                 which_discretization = 'euler',
+                 dtype = torch.float64,
+                 initializer = None):
+        
+        self.step_size = step_size
+        self.beta = beta
+        self.num_particles = num_particles
+        self.dimension = dimension
+        self.which_discretization = which_discretization
+        self.potential_function = potential_function
+
+        if is_valid is None:
+            self.is_valid = lambda particles: torch.ones(len(particles)).to(bool)
+        else:
+            self.is_valid = is_valid
+
+        if initializer is None:
+            self.particles = torch.randn(num_particles, dimension, dtype = dtype, requires_grad = True)
